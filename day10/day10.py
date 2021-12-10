068401c73b050130f4725956c5bfeddd54570299
Jackson Collier <jacksontcollier@gmail.com>
2021-12-10 13:39:18 -0500

    Add day 10 part 2 solution

--- day10/day10.py
+++ day10/day10.py
@@ -4,6 +4,7 @@ opening_chars = set(['(', '[', '{', '<'])
 closing_chars = set([')', ']', '}', '>'])
 opening_to_closing_chars = { '(': ')', '[': ']', '{': '}', '<': '>' }
 char_to_syntax_score = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
+autocomplete_score_map = { ')': 1, ']': 2, '}': 3, '>': 4 }
 
 def parse_input(inputfile):
     fin = open(inputfile)
@@ -18,7 +19,7 @@ def parse_input(inputfile):
 def find_illegal_char(line):
     unclosed_chars = []
     most_recent_open_char = None
-    for i, char in enumerate(line):
+    for char in line:
         if char in opening_chars:
             unclosed_chars.append(char)
             most_recent_open_char = unclosed_chars[len(unclosed_chars)-1]
@@ -37,6 +38,23 @@ def find_illegal_char(line):
 
     return None
 
+def get_closing_chars(incomplete_line):
+    unclosed_chars = []
+    most_recent_open_char = None
+    sequence_closing_chars = ''
+
+    for char in incomplete_line:
+        if char in opening_chars:
+            unclosed_chars.append(char)
+        elif char in closing_chars:
+            unclosed_chars.pop()
+
+    for char in reversed(unclosed_chars):
+        sequence_closing_chars += opening_to_closing_chars[char]
+
+    return sequence_closing_chars
+
+
 def find_illegal_chars(lines):
     illegal_chars = []
 
@@ -51,4 +69,34 @@ def get_illegal_char_syntax_score(lines):
     illegal_chars = find_illegal_chars(lines)
     syntax_scores = [char_to_syntax_score[char] for char in illegal_chars]
     return sum(syntax_scores)
+
+def discard_corrupt_lines(lines):
+    incomplete_lines = []
+
+    for line in lines:
+        illegal_char = find_illegal_char(line)
+        if not illegal_char:
+            incomplete_lines.append(line)
+
+    return incomplete_lines
+
+def get_autocomplete_score(line):
+    closing_chars = get_closing_chars(line)
+    score = 0
+
+    for char in closing_chars:
+        score *= 5
+        score += autocomplete_score_map[char]
+
+    return score
+
+def get_median_autocomplete_score(lines):
+    autocomplete_scores = []
+    lines = discard_corrupt_lines(lines)
+
+    for line in lines:
+        autocomplete_scores.append(get_autocomplete_score(line))
+
+    autocomplete_scores.sort()
+    return autocomplete_scores[int(len(autocomplete_scores) / 2)]
 
--- day10/get_answer.py
+++ day10/get_answer.py
@@ -8,4 +8,7 @@ print("Part One (Test Input): %d" % syntax_score)
 input_lines = parse_input('input_day10.txt')
 syntax_score = get_illegal_char_syntax_score(input_lines)
 print("Part One: %d" % syntax_score)
-
+median_autocomplete_score = get_median_autocomplete_score(test_lines)
+print("Part Two (Test Input): %d" % median_autocomplete_score)
+median_autocomplete_score = get_median_autocomplete_score(input_lines)
+print("Part Two: %d" % median_autocomplete_score)
