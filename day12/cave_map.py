5b76b9effa97ca94eb6c3a4020b2cb3d4997bfca
Jackson Collier <jacksontcollier@gmail.com>
2021-12-12 20:35:33 -0500

    Add day 12 part 2 answer

--- day12/cave_map.py
+++ day12/cave_map.py
@@ -16,12 +16,13 @@ class CaveMap(object):
             else:
                 self.edge_map[end_node] = [start_node]
 
-    def compute_end_paths(self):
+    def compute_end_paths(self, can_visit_small_cave_twice = False):
         cave_path = []
         visited_caves = set()
-        self.dfs('start', cave_path, visited_caves)
+        self.dfs('start', cave_path, visited_caves, can_visit_small_cave_twice)
 
-    def dfs(self, current_node, cave_path, visited_caves):
+    def dfs(self, current_node, cave_path, visited_caves,
+            can_visit_small_cave_twice = False, visited_small_cave_twice = False):
         new_cave_path = cave_path.copy()
         new_visited_caves = visited_caves.copy()
         new_cave_path.append(current_node)
@@ -33,9 +34,15 @@ class CaveMap(object):
 
         for adjacent_cave in self.edge_map[current_node]:
             if adjacent_cave.isupper():
-                self.dfs(adjacent_cave, new_cave_path, new_visited_caves)
+                self.dfs(adjacent_cave, new_cave_path, new_visited_caves,
+                         can_visit_small_cave_twice, visited_small_cave_twice)
             elif not adjacent_cave in new_visited_caves:
-                self.dfs(adjacent_cave, new_cave_path, new_visited_caves)
+                self.dfs(adjacent_cave, new_cave_path, new_visited_caves,
+                         can_visit_small_cave_twice, visited_small_cave_twice)
+            elif can_visit_small_cave_twice and (not visited_small_cave_twice) \
+                 and not (adjacent_cave in ['start', 'end']):
+                self.dfs(adjacent_cave, new_cave_path, new_visited_caves,
+                         can_visit_small_cave_twice, True)
 
     def get_num_end_paths(self):
         return len(self.end_paths)
